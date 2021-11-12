5fc2a675a3922fcf95f20fdcacf0478f06a4064c
Martin Degraeuwe <mdegraeu@z4r10p1.42lyon.fr>
2021-11-12 16:15:25 +0100

    gnl a refaire differemment

--- gnl.c
+++ gnl.c
@@ -6,7 +6,7 @@
 /*   By: mdegraeu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 12:03:19 by mdegraeu          #+#    #+#             */
-/*   Updated: 2021/11/12 15:15:07 by mdegraeu         ###   ########lyon.fr   */
+/*   Updated: 2021/11/12 12:27:03 by mdegraeu         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,79 +52,48 @@ char	*ft_check_reset(char *str)
 	return (str);
 }
 
-char	*ft_strupdate(char *str, char *temp, char buf[BUFFER_SIZE +1])
-{
-	size_t	index;
-
-	//temp = ft_calloc(ft_strlen(str) + 1, sizeof(char));
-	//if (!temp)
-	//	return (NULL);
-	if (ft_strchr(buf, '\n'))
-	{
-		index = ft_strlen(buf) - ft_strlen(ft_strchr(buf, '\n'));
-		temp = str;
-		str = ft_strjoin(str, ft_substr(buf, 0, index + 1));
-		free(temp);
-		temp = ft_strdup(str);
-		return (temp);
-	}
-	else
-	{
-		temp = str;
-		str = ft_strjoin(str, buf);
-		free(temp);
-	}
-	return (str);
-}
-
 char	*get_next_line(int fd)
 {
 	char		buf[BUFFER_SIZE + 1];
 	static char	*str;
 	char		*temp;
-	int			ret;
 	size_t		index;
+	size_t		ret;
 
-
-	if (fd < 0)
+	if (!fd)
 		return (NULL);
-	ret = 1;
+	index = 0;
+	ret = -1;
 	str = ft_check_reset(str);
-	temp = str;
-	while (ret > 0)
+	while (ret != 0)
 	{
 		ret = read(fd, buf, BUFFER_SIZE);
-		if (ft_strlen(buf) == 0)
-			return (NULL);
 		buf[BUFFER_SIZE] = '\0';
 		if (ft_strchr(buf, '\n'))
 		{
 			index = ft_strlen(buf) - ft_strlen(ft_strchr(buf, '\n'));
-			temp = ft_strupdate(str, temp, buf);
-			if (str)
-				free(str);
+			str = ft_strjoin(str, ft_substr(buf, 0, index + 1));
+			temp = str;
 			str = ft_substr(buf, index + 1, ft_strlen(ft_strchr(buf, '\n'))); 
 			return (temp);
 		}
 		else
-		{
-			str = ft_strupdate(str, temp, buf);
-		}
+			str = ft_strjoin(str, buf);
 	}
-	return (NULL);
+	return (str);
 }
-/*
+
 int	main()
 {
 	int		fd;
 	int	i;
 
 	i = 0;
-	fd = open("empty", O_RDONLY);
-	while (i < 6)
+	fd = open("file_test", O_RDONLY);
+	while (i < 4)
 	{
-		printf("%s", get_next_line(fd));
+		printf("%s\n", get_next_line(fd));
 		i++;
 	}
 }
-*/
+
